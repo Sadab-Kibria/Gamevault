e14b71a758d0b9c7098b7bf790a8098d284498c7
Sadab Kibria <sadabkibria123@gmail.com>
2024-12-27 11:15:53 +0600

    first commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,18 @@
-import video3 from "../assets/Asphalt.mp4";
-import video4 from "../assets/Cod.mp4";
+import gif3 from "../assets/gif3.gif";
+import gif2 from "../assets/gif2.gif";
 import gif1 from "../assets/gif1.gif";
 import { AnimatePresence,motion, spring} from 'framer-motion';
 
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20"
-     
     >
       <AnimatePresence>
       <motion.h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide font-extrabold"
           initial={{ translateX:-400 }}
           animate={{ translateX:0 }}
-          transition={{duration:.7,ease:"easeInOut",type:spring}}
-          
-      >
+          exit={{translateX:0}}
+          transition={{duration:.7,ease:"easeInOut",type:spring}}>
          Unlock Your 
         <span className="bg-gradient-to-r from-orange-500 to-red-600 text-transparent bg-clip-text animate-pulse ">
           {" "}
@@ -25,6 +23,7 @@ const HeroSection = () => {
       <motion.p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl overflow-hidden"
       initial={{ translateX:400 }}
       animate={{ translateX:0 }}
+      exit={{translateX:0}}
       transition={{duration:.5,ease:"easeInOut",type:spring}}
       >
         Share your stats, showcase gameplay highlights, and connect with gamers worldwide.
@@ -51,39 +50,17 @@ const HeroSection = () => {
           Documentation
         </motion.a>
       </div>
-      <div className="hidden lg:flex mt-10 justify-center relative"
-      >
-        <div className="absolute size-full inset-0 z-10"></div>
-        <motion.video
-          autoPlay
-          loop
-          muted
-          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-red-400 mx-2 my-4 overflow-hidden"
-            initial={{ translateY:400 }}
-            animate={{ translateY:0 }}
-            transition={{duration:.5}}
-            exit={{ translateY:0 }}
-        >
-          <source src={video4} type="video/mp4" />
-          Your browser does not support the video tag.
-        </motion.video>
-        <motion.video
-          autoPlay
-          loop
-          muted
-          className="rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-red-400 mx-2 my-4 overflow-hidden"
-            initial={{ translateY:400 }}
-            animate={{ translateY:0 }}
-            transition={{duration:.5}}
-            exit={{ translateY:0 }}
-        >
-          <source src={video3} type="video/mp4" />
-          Your browser does not support the video tag.
-        </motion.video>
+      <div className="hidden lg:flex  mt-10 justify-center ">
+        <motion.div className="flex mt-10 justify-center w-full">
+        <img src={gif1} alt="Gif" className="hidden rounded-lg border border-orange-700 shadow-sm shadow-red-400 mx-2 my-4 overflow-hidden lg:block"/>
+      </motion.div>
+      <motion.div className="flex mt-10 justify-center w-full">
+        <img src={gif2} alt="Gif" className="hidden rounded-lg border border-orange-700 shadow-sm shadow-red-400 mx-2 my-4 overflow-hidden lg:block"/>
+      </motion.div>
       </div>
-      <div className="flex mt-10 justify-center relative">
-        <img src={gif1} alt="Gif" className="block rounded-lg w-full border border-orange-700 shadow-sm shadow-red-400 mx-2 my-4 overflow-hidden lg:hidden"/>
-      </div>
+      <motion.div className="flex mt-10 justify-center ">
+        <img src={gif3} alt="Gif" className="block rounded-lg w-full border border-orange-700 shadow-sm shadow-red-400 mx-2 my-4 overflow-hidden lg:hidden"/>
+      </motion.div>
       </AnimatePresence>
     </div>
   );
